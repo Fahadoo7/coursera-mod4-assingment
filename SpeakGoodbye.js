805f661044f16dddb79b43c95c6c5313c32bba5d
syed <shahf2288@gmail.com>
2020-06-17 17:42:28 +0500

    'mod4-assingment'

---
+++ SpeakGoodbye.js
@@ -0,0 +1,8 @@
+(function(window){
+    var goodbyeSpeaker = {};
+        var speakWord = 'Good Bye'
+        goodbyeSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+        }
+        window.goodbyeSpeaker = goodbyeSpeaker;
+})(window);
